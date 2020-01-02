0ffa69dad9864b6297c7e05687250b10e8fc34d3
ravgeetdhillon <ravgeetdhillon@gmail.com>
2020-01-02 19:20:00 +0530

    replaced sass with node-sass

--- compile-sass.sh
+++
@@ -1,3 +0,0 @@
-sass assets/scss/dark-theme.scss assets/css/dark-theme.css
-sass assets/scss/light-theme.scss assets/css/light-theme.css
-sass assets/scss/index.scss assets/css/index.css
--- structurize.sh
+++ structurize.sh
@@ -1,2 +1,6 @@
-mv node_modules/* assets/ -f
-rm -rf node_modules
+dependencies=('bootstrap' 'jquery' 'popper.js' '@fortawesome' 'animate.css' 'moment')
+for dependency in "${dependencies[@]}"
+do
+    rsync -a node_modules/${dependency} assets
+    echo "Moved ${dependency} to assets/ folder"
+done
