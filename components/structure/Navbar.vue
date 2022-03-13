d3bd0293cc7bb85f6a018ebc03016bbdfcb8c8ca
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2022-03-13 16:00:30 +0530

    fix: fixed redirects for resume

--- components/structure/Navbar.vue
+++ components/structure/Navbar.vue
@@ -3,7 +3,14 @@
     <div class="container">
       <div class="row w-lg-75 mx-lg-auto">
         <div
-          class="col-12 d-flex flex-sm-row flex-column justify-content-between align-items-sm-center py-3"
+          class="
+            col-12
+            d-flex
+            flex-sm-row flex-column
+            justify-content-between
+            align-items-sm-center
+            py-3
+          "
         >
           <div class="brand d-flex mr-3 mb-sm-0 mb-3">
             <nuxt-link
@@ -24,12 +31,12 @@
           <ul class="navbar list-unstyled d-flex flex-wrap mb-0 p-0">
             <li v-for="(link, index) in navbarLinks" :key="index" class="ml-3">
               <a
-                v-if="link.external"
+                v-if="link.external || link.static"
                 :href="link.url"
                 class="text-muted text-xs-center text-decoration-none"
               >
                 {{ link.title }}
-                <span v-if="link.title == 'Blog'"><ExternalLink /></span>
+                <span v-if="link.external"><ExternalLink /></span>
               </a>
               <nuxt-link
                 v-else
@@ -37,7 +44,6 @@
                 :to="link.url"
               >
                 {{ link.title }}
-                <span v-if="link.title == 'Blog'"><ExternalLink /></span>
               </nuxt-link>
             </li>
           </ul>
@@ -79,7 +85,8 @@ export default {
         },
         {
           title: 'Resume',
-          url: '/resume',
+          url: '/files/ravgeet-dhillon-resume.pdf',
+          static: true,
         },
       ],
     }
