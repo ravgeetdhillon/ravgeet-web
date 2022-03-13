56d6aa47619ba491517890f0e9d07581ce2a80de
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2022-03-13 15:27:51 +0530

    fix: cleanup jekyll code

--- _scripts/bash/minify-html.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# minify each html file in the build directory
-for i in $(find build -type f -name "*.html");
-do
-    html-minifier --collapse-whitespace --remove-comments --minify-js true $i -o $i
-done
